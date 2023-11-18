bfa99b8fde9de9cf5be87f55381115641308cd67
Camilo Arciniegas Guerrero <63805566+AncientPie@users.noreply.github.com>
2023-11-18 10:29:11 -0500

    Fix(TareaController)

--- src/control/TareaController.java
+++ src/control/TareaController.java
@@ -1,79 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package control;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Menu;
-import javafx.scene.control.TableView;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * FXML Controller class
- *
- * @author aleja
- */
 public class TareaController implements Initializable {
 
     @FXML
-    private ComboBox<?> cboBase;
+    private ComboBox<String> cboBase;
     @FXML
     private ComboBox<?> cboTabla;
-    @FXML
-    private TableView<?> tblTarea;
-    @FXML
-    private Menu menItemSalir;
-    @FXML
-    private AnchorPane ancTarea;
 
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        
+    }
+    
+    /* private void populateDatabases() {
+        try {
+            
+            //No hay conexion de mysql en este punto
+            
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SHOW DATABASES");
 
+            List<String> databases = new ArrayList<>();
+            while (resultSet.next()) {
+                databases.add(resultSet.getString(1));
+            }
+
+            cboBase.getItems().addAll(databases);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    } */
+    
     @FXML
     private void doBaseMostrar(ActionEvent event) {
     }
 
     @FXML
     private void doTablaMostrar(ActionEvent event) {
-    }
-
-    @FXML
-    private void doSalir(ActionEvent event) {
-    try
-        {
-            Stage stage=new Stage();
-            FXMLLoader loader= new FXMLLoader(getClass().getResource("/vista/Unravel.fxml"));
-            Parent root=loader.load();
-            Scene scene=new Scene (root);
-            
-            stage.setTitle("Unravel-a-data");
-            stage.setScene(scene);
-            stage.show();
-            var myStage=(Stage)this.ancTarea.getScene().getWindow();
-            myStage.close();
-        }
-        catch(IOException ex)
-        {
-            Logger.getLogger(UnravelController.class.getName()).log(Level.SEVERE,null,ex);
-        }
     }
     
 }
